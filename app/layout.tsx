20d326e3259c135f6d0947c8fcdbe831a2f68bd1
devhmars <dev.marshallharvey@gmail.com>
2024-05-23 14:23:28 +0100

    added in new top section mesh gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { memo, SVGProps } from 'react';
+import { Ellipse1Icon } from '@/components/mesh-gradient/Ellipse1Icon';
+import { Ellipse2Icon } from '@/components/mesh-gradient/Ellipse2Icon';
+import { Vector1Icon } from "@/components/mesh-gradient/Vector1Icon";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +22,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+      <div className={"absolute z-10 w-[100%] h-[474px] column flex-start bg-[#56fbb6] overflow-hidden blur-[92.69999694824219px]"}>
+        <div className={"absolute z-20 left-[-81px] top-[-180px] w-[669px] h-[417px] overflow-visible"}>
+          <Ellipse1Icon className={"w-[100%] h-[100%]"} />
+        </div>
+        <div className={"absolute left-[859px] top-[176px] w-[900px] h-[982px] overflow-visible"}>
+          <Ellipse2Icon className={"w-[100%] h-[100%]"} />
+        </div>
+        <div className="absolute left-[489.2708px] top-[123.5381px] w-[494.1668px] h-[637.0844px] bg-[#8f1491] origin-top-left" style={{ transform: 'matrix(0.6272, 0.7789, -0.7789, 0.6272, 0, 0)'}}></div>
+        <div className="absolute left-[710.8736px] top-[-54px] w-[782.1264px] h-[285.2603px] overflow-visible" 
+              style={{ filter: 'drop-shadow(0px 4px 4px #00000040)' }}>
+          <Vector1Icon className={"w-[100%] h-[100%]"} />
+        </div>
+      </div>
         <Header />
         {children}
       </body>
